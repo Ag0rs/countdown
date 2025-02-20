cfb59002e492ffc3b27952e5d216c22e7afab229
Ag0rs <sashakovalchuk250@gmail.com>
2025-02-20 16:38:29 +0200

    creating a timer clock

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,31 @@ document.addEventListener("DOMContentLoaded", function () {
         sidebar.classList.toggle("open");
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    function updateTimer() {
+        const now = new Date();
+        const hours = String(now.getHours()).padStart(2, "0");
+        const minutes = String(now.getMinutes()).padStart(2, "0");
+        const seconds = String(now.getSeconds()).padStart(2, "0");
+
+        document.getElementById("hours").textContent = hours;
+        document.getElementById("minutes").textContent = minutes;
+        document.getElementById("seconds").textContent = seconds;
+    }
+
+    setInterval(updateTimer, 1000);
+    updateTimer(); 
+});
+
+function updateDate() {
+    const dates = document.querySelector(".date");
+    if (!dates) return;
+
+    const now = new Date();
+    const options = { year: "numeric", month: "numeric", day: "numeric" };
+    dates.textContent = now.toLocaleDateString("uk-UA", options);
+}
+
+document.addEventListener("DOMContentLoaded", updateDate);
+
