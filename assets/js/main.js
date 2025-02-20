d5caa4ed540249887dafc6960f4b8e1e364255d6
Ag0rs <sashakovalchuk250@gmail.com>
2025-02-20 14:42:51 +0200

    created the project and the sidebar

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const sidebar = document.querySelector(".sidebar");
+    const menuToggle = document.querySelector(".menu-toggle");
+
+    menuToggle.addEventListener("click", function () {
+        sidebar.classList.toggle("open");
+    });
+});
